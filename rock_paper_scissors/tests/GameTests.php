310e796cce7784d2aae189068944af31f681bb52
Ike Mattice & Kevin Tokheim <student@epicodus.com>
2015-08-12 09:49:05 -0700

    add initial specs for function testing

---
+++ rock_paper_scissors/tests/GameTests.php
@@ -0,0 +1,31 @@
+//User inputs rock, computer inputs paper; paper wins
+//input->
+//output->
+
+//User inputs scissors, computer inputs rock; rock wins
+//input->
+//output->
+
+//User inputs paper, computer inputs scissors; scissors wins
+//input->
+//output->
+
+//User inputs rock, computer inputs scissors; rock wins
+//input->
+//output->
+
+//User inputs scissors, computer inputs paper; scissors wins
+//input->
+//output->
+
+//User inputs paper, computer inputs rock; paper wins
+//input->
+//output->
+
+//User inputs matches computer input; tie -> no winner
+//input->
+//output->
+
+//Computer pulls random number to decide computer choice
+//input->
+//output->
